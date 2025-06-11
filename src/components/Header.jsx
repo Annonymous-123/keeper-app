b230e904f3c024d2483b604a482b025344c390c4
Ann Mary Augustine <annaugustinemary@gmail.com>
2025-06-11 12:05:42 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import NoteAltIcon from '@mui/icons-material/NoteAlt';
 function Header() {
   return (
     <header>
-      <h1><NoteAltIcon/> Keeper</h1>
+      <h1><NoteAltIcon/> Notes</h1>
     </header>
   );
 }
